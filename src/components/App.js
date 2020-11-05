fb48f7254894c99c340100081ba4747d090b7ff0
Your Name <you@example.com>
2020-11-05 13:35:17 +0300

    Project was created

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app.css";
+
+function App() {
+  const apiURL = "https://www.anapioficeandfire.com/api/books?pageSize=30";
+
+  return (
+    <div className="App">
+      <h1>Game of Thrones Books</h1>
+      <h2>Fetch a list from an API and display it</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button">Fetch Data</button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+
+      {/* Use JSX below for each book */}
+      <div className="books">
+        <div className="book">
+          <h3>Book Number</h3>
+          <h2>Book Name</h2>
+
+          <div className="details">
+            <p>👨: Author/Authors</p>
+            <p>📖: Number of pages</p>
+            <p>🏘️: Book Country</p>
+            <p>⏰: Release date</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
